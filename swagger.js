427035993c34651c6fa618fa131f37a4acaff894
GlitchyRobot <glitchyrobot998@gmail.com>
2024-06-22 11:22:07 -0700

    finalized

---
+++ swagger.js
@@ -0,0 +1,18 @@
+require("dotenv")
+const swaggerAutogen = require('swagger-autogen')();
+
+const doc = {
+  info: {
+    title: 'My API',
+    description: 'Description'
+  },
+  host: `https://casey-cse341-24.onrender.com/contacts`
+};
+
+const outputFile = './swagger-output.json';
+const routes = ["./routes/contacts.js"];
+
+/* NOTE: If you are using the express Router, you must pass in the 'routes' only the 
+root file where the route starts, such as index.js, app.js, routes.js, etc ... */
+
+swaggerAutogen(outputFile, routes, doc);
